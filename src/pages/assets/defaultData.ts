04c6c8c201485b8dbed0d82a8738f1abfdde38b5
David Lopez <3936234+davidnoelopez@users.noreply.github.com>
2023-07-20 11:36:20 -0600

    Changed DefaultData to TS

---
+++ src/pages/assets/defaultData.ts
@@ -0,0 +1,66 @@
+import { DefaultData } from "~/types";
+
+export const defaultData: DefaultData = {
+  languages: [
+    {
+      key: "mx",
+      name: "Español",
+      locale: "es-MX",
+    },
+    {
+      key: "us",
+      name: "English",
+      locale: "en-US",
+    },
+  ],
+  brandName: [
+    {
+      locale: "es-MX",
+      text: "Licencias y Permisos",
+    },
+    {
+      locale: "en-US",
+      text: "Licencias y Permisos",
+    },
+  ],
+  titles: [
+    {
+      locale: "es-MX",
+      text: "El mejor gestor de permisos en México.",
+    },
+    {
+      locale: "en-US",
+      text: "The best Permits Manager in Mexico.",
+    },
+  ],
+  serviceDescription: [
+    {
+      locale: "es-MX",
+      text: "Somos el grupo de gestores de trámites y permisos más confiable y veloz en México. Contamos con más de 30 años de experiencia. Obtén tus permisos de manera ágil, segura y sin complicaciones. Lo tramitamos por ti.",
+    },
+    {
+      locale: "en-US",
+      text: "We are the most reliable and fast group of administrative managers for procedures and permits in Mexico. We have over 30 years of experience. Obtain your permits quickly, securely, and hassle-free. We handle the process for you.",
+    },
+  ],
+  callToAction: [
+    {
+      locale: "es-MX",
+      text: "Contáctanos para recibir sin costo una auditoria del estatus real de tu negocio.",
+    },
+    {
+      locale: "en-US",
+      text: "Contact us to receive a free audit of the actual status of your business.",
+    },
+  ],
+  contactButtonText: [
+    {
+      locale: "es-MX",
+      text: "Contactanos",
+    },
+    {
+      locale: "en-US",
+      text: "Contact us",
+    },
+  ],
+};
