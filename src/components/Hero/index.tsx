55ba7bd81c053c8cd75628c8daaaa6d89b199810
David Lopez <davidnoelopez@gmail.com>
2023-07-24 22:42:24 -0600

    Added bg to layout

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -21,88 +21,32 @@ export default function Hero({ defaultData }: Props) {
   )[0]?.text;
 
   return (
-    <div className="h-full bg-white dark:bg-gray-900">
-      <div className="relative isolate px-6 pt-0 md:px-8">
-        <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
-          <svg
-            className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
-            viewBox="0 0 1155 678"
+    <div className="mx-auto max-w-2xl py-32 sm:py-48 md:py-48">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-200 sm:text-6xl ">
+          {brandName}
+        </h1>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-200 sm:text-4xl">
+          {title}
+        </h2>
+        <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-500">
+          {serviceDescription}
+        </p>
+        <div className="mt-8 flex flex-col items-center justify-center space-y-4 px-28">
+          <p className="text-lg font-bold leading-8 text-gray-600 dark:text-gray-500">
+            {callToAction}
+          </p>
+          <Link
+            href="/contact"
+            className="w-fit rounded-md bg-blue-800 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
           >
-            <path
-              fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
-              fillOpacity=".3"
-              d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-            />
-            <defs>
-              <linearGradient
-                id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
-                x1="1155.49"
-                x2="-78.208"
-                y1=".177"
-                y2="474.645"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop stopColor="#9089FC" />
-                <stop offset={1} stopColor="#FF80B5" />
-              </linearGradient>
-            </defs>
-          </svg>
+            {
+              defaultData.contactButtonText.filter(
+                (t) => t.locale === locale
+              )[0]?.text
+            }
+          </Link>
         </div>
-
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 md:py-48">
-          <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-200 sm:text-6xl ">
-              {brandName}
-            </h1>
-            <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-200 sm:text-4xl">
-              {title}
-            </h2>
-            <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-500">
-              {serviceDescription}
-            </p>
-            <div className="mt-8 flex flex-col items-center justify-center space-y-4 px-28">
-              <p className="text-lg font-bold leading-8 text-gray-600 dark:text-gray-500">
-                {callToAction}
-              </p>
-              <Link
-                href="/contact"
-                className="w-fit rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                {
-                  defaultData.contactButtonText.filter(
-                    (t) => t.locale === locale
-                  )[0]?.text
-                }
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className="absolute inset-x-0 top-[calc(50%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl">
-          <svg
-            className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
-            viewBox="0 0 1155 678"
-          >
-            <path
-              fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
-              fillOpacity=".3"
-              d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-            />
-            <defs>
-              <linearGradient
-                id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
-                x1="1155.49"
-                x2="-78.208"
-                y1=".177"
-                y2="474.645"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop stopColor="#9089FC" />
-                <stop offset={1} stopColor="#FF80B5" />
-              </linearGradient>
-            </defs>
-          </svg>
-        </div>
-        <Services />
       </div>
     </div>
   );
