cef7524936fc84de8de7f556e7f120d826598828
David Lopez <davidnoelopez@gmail.com>
2023-07-24 22:43:56 -0600

    Added clients page

---
+++ src/components/Home/Clients.tsx
@@ -0,0 +1,105 @@
+import Image from "next/image";
+import React from "react";
+
+const ClientsData = [
+  {
+    name: "Pollo Maton",
+    logo: "/assets/clientsLogos/pollo_maton.webp",
+  },
+  {
+    name: "Grupo GP",
+    logo: "/assets/clientsLogos/grupo_gp.webp",
+  },
+  {
+    name: "Centro Medico Eternal",
+    logo: "/assets/clientsLogos/eternal.webp",
+  },
+  {
+    name: "Christus Muguerza",
+    logo: "/assets/clientsLogos/christus_muguerza.webp",
+  },
+  {
+    name: "Centro Medico Sancen",
+    logo: "/assets/clientsLogos/sancen.webp",
+  },
+  {
+    name: "Orage Investments",
+    logo: "/assets/clientsLogos/orange_investments.webp",
+  },
+  {
+    name: "Posadas",
+    logo: "/assets/clientsLogos/posadas.webp",
+  },
+  {
+    name: "Las Alitas",
+    logo: "/assets/clientsLogos/las_alitas.webp",
+  },
+  {
+    name: "Noreste Grill",
+    logo: "/assets/clientsLogos/noreste_grill.webp",
+  },
+  {
+    name: "Live Aqua",
+    logo: "/assets/clientsLogos/live_aqua.webp",
+  },
+  {
+    name: "Delicias del Contry",
+    logo: "/assets/clientsLogos/delicias.webp",
+  },
+  {
+    name: "Soriana",
+    logo: "/assets/clientsLogos/soriana.webp",
+  },
+  {
+    name: "Acostaverde",
+    logo: "/assets/clientsLogos/acostaverde.webp",
+  },
+  {
+    name: "Plani Grupo",
+    logo: "/assets/clientsLogos/plani_grupo.webp",
+  },
+];
+
+const Clients = () => {
+  return (
+    <div id="clients" className="relative flex gap-10 overflow-hidden pb-10">
+      <div className="flex min-w-full shrink-0 animate-marquee items-center justify-around gap-8">
+        {ClientsData.map((client, index) => (
+          <div
+            key={index}
+            className="inline-block h-48 w-48 max-w-xs overflow-hidden rounded-lg bg-white text-center align-middle shadow-lg shadow-slate-400 transition-shadow duration-300 ease-in-out hover:bg-gray-100 hover:shadow-xl hover:shadow-slate-400 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+          >
+            <Image
+              src={client.logo}
+              alt={client.name}
+              width={200}
+              height={200}
+              quality={100}
+            />
+          </div>
+        ))}
+      </div>
+      <div
+        aria-hidden="true"
+        className="flex min-w-full shrink-0 animate-marquee items-center justify-around gap-8"
+      >
+        {ClientsData.map((client, index) => (
+          <div
+            key={index}
+            className="inline-block h-48 w-48 max-w-xs overflow-hidden rounded-lg bg-white text-center align-middle shadow-lg shadow-slate-400 transition-shadow duration-300 ease-in-out hover:bg-gray-100 hover:shadow-xl hover:shadow-slate-400 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+          >
+            <Image
+              src={client.logo}
+              alt={client.name}
+              width={200}
+              height={200}
+              quality={100}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Clients;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import Hero from "../components/Hero";
 import { defaultData } from "~/utils/assets/defaultData";
 import { DefaultData } from "~/types";
 import Services from "~/components/Home/Services";
+import Clients from "~/components/Home/Clients";
 
 function Home() {
   return (
     <div className="h-full">
       <Hero defaultData={defaultData as DefaultData} />
       <Services />
+      <Clients />
     </div>
   );
 }
