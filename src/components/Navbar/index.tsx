c41139431bba8effb1338eb17badf91548f41254
David Lopez <davidnoelopez@gmail.com>
2023-07-30 22:59:55 -0600

    Update index.tsx

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -62,7 +62,7 @@ const index = ({ defaultData }: Props) => {
         <div className="flex md:hidden">
           <Link
             href="/contact"
-            className="mr-4 rounded-md bg-indigo-600 px-3.5 py-2.5 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="mr-4 rounded-md bg-blue-800 px-3.5 py-2.5 text-xs font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
           >
             {
               defaultData.contactButtonText.filter(
