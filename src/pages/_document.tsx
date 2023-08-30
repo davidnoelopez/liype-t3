528b3beac8f49720a943f558b8cdfd25d681cdd5
David Lopez <davidnoelopez@gmail.com>
2023-08-30 14:10:21 -0600

    Changed GTM script strategy

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html style={{ scrollBehavior: "smooth" }}>
       <Head>
-        <Script id="gtm" strategy="lazyOnload">
+        <Script id="gtm" strategy="beforeInteractive">
           {`
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
