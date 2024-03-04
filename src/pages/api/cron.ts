d8dd44d18d3771a0764488205057a8ff1904877c
David Lopez <davidnoelopez@gmail.com>
2024-03-04 11:27:26 -0600

    Refactor cron job endpoint

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -1,27 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { Resend } from "resend";
-import { env } from "~/env.mjs";
-import Email from "~/server/emails";
 
 export default async function handler(
   _req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const resend = new Resend(env.RESEND_API_KEY);
-
-  await resend
-    .sendEmail({
-      from: "Nuevo Registro <no-reply@nogiistudio.com>",
-      to: ["david@nogiistudio.com"],
-      subject: "Prueba de cron",
-      react: Email(),
-    })
-    .then((response) => {
-      console.log(response);
-      res.status(200).end("Cron LIYPE!");
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).end("Error en el cron");
-    });
+  res.status(200).json({ message: "Cron job ran successfully" });
 }
