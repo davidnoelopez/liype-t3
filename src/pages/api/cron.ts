5541e0ff7a35721e03eade9b4487f6a025a6098f
David Lopez <davidnoelopez@gmail.com>
2024-03-04 11:14:15 -0600

    Refactor email sending in cron job

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -6,11 +6,19 @@ import Email from "~/server/emails";
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
   const resend = new Resend(env.RESEND_API_KEY);
 
-  resend.sendEmail({
-    from: "Nuevo Registro <no-reply@nogiistudio.com>",
-    to: ["david@nogiistudio.com"],
-    subject: "Prueba de cron",
-    react: Email(),
-  });
+  resend
+    .sendEmail({
+      from: "Nuevo Registro <no-reply@nogiistudio.com>",
+      to: ["david@nogiistudio.com"],
+      subject: "Prueba de cron",
+      react: Email(),
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
   res.status(200).end("Cron LIYPE!");
 }
