e4270dff2d08e1dc7a5ebc156afb38c85d4390c3
David Lopez <davidnoelopez@gmail.com>
2024-03-04 11:01:26 -0600

    Add cron job to send test email

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -1,5 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Resend } from "resend";
+import { env } from "~/env.mjs";
+import Email from "~/server/emails";
 
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).end("Hello Cron!");
+  const resend = new Resend(env.RESEND_API_KEY);
+
+  resend.sendEmail({
+    from: "Nuevo Registro <no-reply@nogiistudio.com>",
+    to: ["david@nogiistudio.com"],
+    subject: "Prueba de cron",
+    react: Email(),
+  });
+  res.status(200).end("Cron LIYPE!");
 }
